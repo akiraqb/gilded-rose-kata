2b5671a34377cdca6bb65b5389cdeddae8736fae
akiraqb <a.szostak.uk@gmail.com>
2019-05-30 18:10:00 +0100

    Added argument to GildedRose []Istems to enable tests + _test first test for main + GildedRose function 53% coverage

--- go/gilded-rose_test.go
+++ go/gilded-rose_test.go
@@ -3,5 +3,20 @@ package main
 import "testing"
 
 func Test_GildedRose(t *testing.T) {
-	main()
+	
+
+	var items = []Item{
+		Item{"+5 Dexterity Vest", 10, 20},
+		Item{"Aged Brie", 2, 0},
+		Item{"Elixir of the Mongoose", 5, 7},
+		Item{"Sulfuras, Hand of Ragnaros", 0, 80},
+		Item{"Backstage passes to a TAFKAL80ETC concert", 15, 20},
+		Item{"Conjured Mana Cake", 3, 6},
+	}
+
+
+
+	t.Run("Test_main", func(t *testing.T){ main() })
+	t.Run("First_test",func(t *testing.T){ GildedRose(items) })
+
 }
